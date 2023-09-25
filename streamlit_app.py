09d4231d220a9618873d11cdc4d2efc6d10fb152
liana-simopoulos <145505699+liana-simopoulos@users.noreply.github.com>
2023-09-25 14:29:08 -0400

    syntax error

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows= get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-def insert_row_snowflake(new fruit):
+def insert_row_snowflake(new_fruit):
     with mycnx.cursor() as my_cur:
         my_cur.execute("insert into fruit_load_list values ('from streamlit')")
        return 'Thanks for adding '+ new_fruit
